cf5e0078c5d8d72e2674b0e24a5c6cd388cde8e3
vrssreebhagavathi-lang <vrssreebhagavathi@gmail.com>
2026-04-18 01:26:36 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,12 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
-
-  if (hamburger && navMenu) {
-    hamburger.addEventListener("click", () => {
-      navMenu.classList.toggle("active");
-    });
-  }
-
-});
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+}

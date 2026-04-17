a7861a3f2a8ded280df27eab4fa78ca3894ba719
vrssreebhagavathi-lang <vrssreebhagavathi@gmail.com>
2026-04-18 01:18:33 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  }
+
+});
